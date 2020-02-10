a8c11232163e67d4df2389b9f9e5580d1c00633e
hayase <tear.drive.1996@gmail.com>
2020-02-10 18:46:56 +0900

    sh

---
+++ a.sh
@@ -0,0 +1,2 @@
+cd build/classes/java/main
+mpirun -np 2 -machinefile ../../../../machines java -cp /usr/local/lib/mpj-v0_44/lib/mpj.jar:. -Djava.library.path=/usr/local/lib/mpj-v0_44/lib app.App 0 0 native /hogehoge 1024 $((2**23))
